3d5f8fe07f6aadb52000f73869fe7ba43bf691e4
ZMAForge <zeldamarieasvitt@outlook.com>
2026-09-22 10:14:39 -0600

    assignment1Completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        
+        Console.Write("What is your First name? ");
+        string fName = Console.ReadLine();
+        
+        Console.Write("What is your Last name? ");
+        string lName = Console.ReadLine();
+
+        Console.WriteLine($" Your name is {lName}, {fName} {lName}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,55 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your percentage in your class? ");
+        int grade = int.Parse(Console.ReadLine());
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        int key = grade % 10;
+        string sign = "";
+        if (key >= 7 && letter != "A" && letter != "F")
+        {
+            sign = "+";
+        }
+        else if (key < 3 && letter != "F")
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+        
+        Console.WriteLine($"Your Grade is {letter}{sign}");
+
+        if (grade > 70)
+        {
+            Console.WriteLine("You have Passed your class! Congratulations!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass this time.. Keep studying and you will have it next time!");
+        }
     }
 }
